e970b05b8c54c8ab3d4c36c4aaeed49894516c69
canpinaronline <64422728+canpinaronline@users.noreply.github.com>
2022-10-14 16:12:35 +0300

    Delete dom/create_elements directory

--- dom/create_elements/app.js
+++
@@ -1,9 +0,0 @@
-// Element oluşturma
-const li = document.createElement('li');
-
-// Text node oluşturma ve ekleme.
-// appendChild method added text li element's inner.
-li.appendChild(document.createTextNode('I added this task.'))
-
-// Listeyi ul içine ekleme.
-const ul = document.querySelector('ul').appendChild(li);
